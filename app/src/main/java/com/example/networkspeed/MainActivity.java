b43e21a2b0f8bda1ddfa58cc42e539c351b31bb5
bob <mohammed.lamnaour@gmail.com>
2022-04-27 15:53:32 +0000

    EFM TDM 2018.

--- app/src/main/java/com/example/networkspeed/MainActivity.java
+++ app/src/main/java/com/example/networkspeed/MainActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2;
     EditText e1,e2,e3,e4;
     ImageView im;
+    int actualAng = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +34,52 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Integer debit = Integer.valueOf(e1.getText().toString());
+                calculer(debit);
+                e2.setText(String.valueOf(debit*0.75));
+                e3.setText(String.valueOf(debit*0.15));
+                e4.setText(String.valueOf(debit*0.10));
             }
         });
 
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                e1.getText().clear();
+                e2.getText().clear();
+                e3.getText().clear();
+                e4.getText().clear();
 
+                rotation(actualAng,0);
             }
         });
+
+    }
+
+
+    public void rotation(int ang1, int ang2){
+        RotateAnimation rotation = new RotateAnimation(ang1, ang2,RotateAnimation.RELATIVE_TO_SELF,0.7f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        rotation.setFillAfter(true);
+        rotation.setDuration(2000);
+        im.startAnimation(rotation);
+    }
+
+    public void calculer(int debit){
+        if(debit<0){
+            Toast.makeText(this, "Erreur! le debit doit etre entre 0 et 100Mb/s", Toast.LENGTH_SHORT).show();
+            rotation(actualAng,0);
+            actualAng = 0;
+            return;
+        }
+        if(debit>100){
+            Toast.makeText(this, "Erreur! le debit doit etre entre 0 et 100Mb/s", Toast.LENGTH_SHORT).show();
+            rotation(actualAng,180);
+            actualAng = 180;
+            return;
+        }
+
+        int angle = 180 * debit/100;
+        rotation(actualAng,angle);
+        actualAng = angle;
     }
 }
